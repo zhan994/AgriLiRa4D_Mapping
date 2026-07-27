64e29d2b53c854e770fbecd8c3ed0c9b2990b140
Zhihao Zhan <zhanzhihao_dt@163.com>
2026-07-27 16:44:11 +0800

    add mapper

---
+++ include/mapper.h
@@ -0,0 +1,22 @@
+/**
+ * @file mapper.h
+ * @author Zhihao Zhan (zhihazhan2-c@my.cityu.edu.hk)
+ * @brief mapper for AgriLiRa4D
+ * @version 0.1
+ * @date 2026-07-27
+ *
+ * @copyright Copyright (c) 2026
+ *
+ */
+
+#ifndef MAPPER_H
+#define MAPPER_H
+
+namespace mapping {
+class Mapper {
+public:
+private:
+};
+} // namespace mapping
+
+#endif // MAPPER_H
---
+++ src/mapper.cpp
@@ -0,0 +1,5 @@
+#include "mapper.h"
+
+namespace mapping {
+  
+}
